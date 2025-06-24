2ae9544091e75b6c9db34228a958397b35934e38
Omnipotara <ognjenpavlovic44@Gmail.com>
2025-06-24 13:20:35 +0200

    cors popravljen kako bi front-end radio

--- config/cors.php
+++ config/cors.php
@@ -29,10 +29,7 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
-    'paths' => ['api/*'],
-
-    'allowed_origins' => ['http://localhost:3000/'],
 
 ];
