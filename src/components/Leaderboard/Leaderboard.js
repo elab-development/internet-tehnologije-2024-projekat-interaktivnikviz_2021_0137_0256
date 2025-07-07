bf256b328331fc5f950b223a3c46677291cfdaf8
Omnipotara <ognjenpavlovic44@Gmail.com>
2025-07-07 23:09:48 +0200

    dodata export funkcija za leaderboard

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -6,9 +6,12 @@ const Leaderboard = () => {
   const [leaders, setLeaders] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
     const token = localStorage.getItem('token');
+
+    // Dohvatanje leaderboarda
     axios.get('http://127.0.0.1:8000/api/leaderboards', {
             headers: {
                 Authorization: `Bearer ${token}`,
@@ -25,10 +28,44 @@ const Leaderboard = () => {
         setError('Greška prilikom učitavanja leaderboarda');
         setLoading(false);
       });
+
+    // Provera da li je admin
+    axios.get('http://127.0.0.1:8000/api/admin/profile', {
+          headers: {
+              Authorization: `Bearer ${token}`,
+          }
+  })
+    .then(() => setIsAdmin(true))
+    .catch(() => setIsAdmin(false));
   }, []);
+  
 
   if (loading) return <p>Učitavanje...</p>;
   if (error) return <p>{error}</p>;
+
+  // Ova funkcija će se pozvati kada korisnik klikne na dugme za eksportovanje
+  const handleExport = () => {
+    const token = localStorage.getItem('token');
+    axios.get('http://127.0.0.1:8000/api/leaderboards-export', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Accept': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+      },
+      responseType: 'blob' // Ovaj deo je važan za preuzimanje datoteke jer nam govori da očekujemo binarni odgovor
+    })
+    .then(response => {
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'leaderboard.xlsx');
+      document.body.appendChild(link);
+      link.click();
+    })
+    .catch(err => {
+      console.error('Greška prilikom eksportovanja:', err);
+      alert('Nešto nije u redu prilikom eksportovanja.');
+    });
+  };  
 
   return (
     <div className={styles.leaderboard}>
@@ -40,6 +77,12 @@ const Leaderboard = () => {
           </li>
         ))}
       </ul>
+      {isAdmin && (
+        <button onClick={handleExport} className={styles.exportButton}>
+            Exportuj u Excel
+        </button>
+      )}
+
     </div>
   );
 };
